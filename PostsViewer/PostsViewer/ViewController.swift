a712711b8197c2ebbd16be7020b4288076f600f4
dhanunjaykumar <dhanunjaykumar@users.noreply.github.com>
2024-04-25 16:29:36 +0530

    New project setup

---
+++ PostsViewer/PostsViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsViewer
+//
+//  Created by Dhanunjay Kumar on 25/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
